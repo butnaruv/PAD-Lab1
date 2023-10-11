a97abd5138378a21800bc6e8bdb1b1645f9bd2c1
butnaruv <butnaru.valeria05@gmail.com>
2023-10-11 12:25:09 +0300

    Add event management service

---
+++ EventManagementService/EventManagementService/Services/CommunicationService.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Core;
+using System.Threading.Tasks;
+namespace EventManagementService.Services
+{
+
+    public class CommunicationService : Communication.CommunicationBase
+    {
+        public override Task<MessageResponse> SendMessage(MessageRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new MessageResponse { Message = "I got it, you are task manager" });
+        }
+    }
+
+}
---
+++ EventManagementService/EventManagementService/Services/EventManagementClient.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Net.Client;
+
+namespace EventManagementService.Services
+{
+    public class EventManager
+    {
+        public void SendMessage() {
+            var channel = GrpcChannel.ForAddress("https://localhost:7206");
+            var client = new Communication.CommunicationClient(channel);
+            var response = client.SendMessage(new MessageRequest { Message = "I got it, you are task manager" });
+        }
+        
+
+    }
+}
