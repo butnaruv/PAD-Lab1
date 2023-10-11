d1808b81c3ea671a2a9eb2f417faed9f49205a46
butnaruv <butnaru.valeria05@gmail.com>
2023-10-11 12:26:32 +0300

    Add task management service

---
+++ TaskManagementService/TaskManagementService/Services/CommunicationService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+using System.Threading.Tasks;
+
+namespace TaskManagementService.Services
+{
+  
+
+    public class CommunicationService : Communication.CommunicationBase
+    {
+        public override Task<MessageResponse> SendMessage(MessageRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new MessageResponse { Message = "I am task manager" });
+        }
+    }
+
+}
---
+++ TaskManagementService/TaskManagementService/Services/TaskManagementService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+
+namespace TaskManagementService.Services
+{
+    public class TaskManager
+    {
+        public static void SendMessageToEventService()
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:7206");
+            var client = new Communication.CommunicationClient(channel);
+            var response = client.SendMessage(new MessageRequest { Message = "I am task manager" });
+
+            // Process the response from the EventManagementService
+            Console.WriteLine(response.Message);
+        }
+    }
+}
