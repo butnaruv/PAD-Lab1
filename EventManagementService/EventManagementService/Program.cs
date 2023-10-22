312ae63d1f031a4133d72b5e4c7a4f7b6bdd6162
butnaruv <butnaru.valeria05@gmail.com>
2023-10-22 12:48:59 +0300

    Add status endpoint for event manager service

--- EventManagementService/EventManagementService/Program.cs
+++ EventManagementService/EventManagementService/Program.cs
@@ -1,5 +1,6 @@
 using EventManagementService.Data;
 using EventManagementService.Repositories;
+using EventManagementService.Services;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 
@@ -14,14 +15,12 @@ builder.Services.AddGrpc();
 builder.Services.AddScoped<IEventRepository, EventRepository>();
 builder.Services.AddDbContext<DbContextClass>();
 IServiceCollection serviceCollection = builder.Services.AddAutoMapper(typeof(Program).Assembly);
-//builder.Services.AddGrpcHealthChecks()
-//                .AddCheck("Sample", () => HealthCheckResult.Healthy());
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<EventManagementService.Services.EventService>();
-//app.MapGrpcHealthChecksService();
+app.MapGrpcService<EventService>();
+app.MapGrpcService<CheckHealthService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
