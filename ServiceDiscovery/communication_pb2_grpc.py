a88768d632519774d224b24d9757aed94b3ebb28
butnaruv <butnaru.valeria05@gmail.com>
2023-11-23 01:12:39 +0200

    Add service discovery

--- ServiceDiscovery/communication_pb2_grpc.py
+++ ServiceDiscovery/communication_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import generated_from_proto.communication_pb2 as communication__pb2
+import communication_pb2 as communication__pb2
 
 
 class CommunicationStub(object):
---
+++ ServiceDiscovery/main.py
@@ -0,0 +1,40 @@
+# This is a sample Python script.
+import grpc
+from concurrent import futures
+import communication_pb2
+import communication_pb2_grpc
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+class CommunicationServicer(communication_pb2_grpc.CommunicationServicer):
+
+    def SendMessage(self, request, context):
+        print(request.sender)
+        print(request.message)
+        with grpc.insecure_channel('localhost:5003') as channel:
+            stub = communication_pb2_grpc.CommunicationStub(channel)
+            requestObject = communication_pb2.MessageRequest(sender = request.sender,
+                                                             message=request.message)
+            response = stub.SendMessage(requestObject)
+
+        hello_reply = communication_pb2.MessageResponse()
+        hello_reply.message = response.message
+        print()
+        return hello_reply
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    communication_pb2_grpc.add_CommunicationServicer_to_server(CommunicationServicer(), server)
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    server.wait_for_termination()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    serve()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
